72e4b9b13330e95544f6e2484d4d5dc1b077512b
mugash01 <johnkmugambi95@gmail.com>
2023-03-20 14:02:44 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+
+	for (i = 26; i > 0; i--)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
